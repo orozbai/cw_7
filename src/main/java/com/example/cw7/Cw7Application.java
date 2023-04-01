87fba39c40b046f4b77ac48df42e187e9b69fac9
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-04-01 11:24:21 +0600

    gti init

---
+++ src/main/java/com/example/cw7/Cw7Application.java
@@ -0,0 +1,13 @@
+package com.example.cw7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cw7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cw7Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cw7/Cw7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cw7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cw7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
